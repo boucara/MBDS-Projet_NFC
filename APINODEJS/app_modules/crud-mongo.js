31307b5032ff050a729ae5f98f66b560b3b32328
boucara <traorebelco@gmail.com>
2018-03-28 23:57:11 +0200

    API node js

---
+++ APINODEJS/app_modules/crud-mongo.js
@@ -0,0 +1,200 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
+
+exports.connexionMongo = function(callback) {
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        callback(err, db);
+    });
+};
+
+exports.checkURL = function(params, callback) {
+    MongoClient.connect(url, function(err, db) {
+        if(!err) {
+            // La requete mongoDB
+            db.collection("videos")
+                .find(params).toArray(function(err, data) {
+                    var reponse;
+
+                    if(!err){
+                        var result = data.length>0?true:false;
+                        reponse = {
+                            succes: true,
+                            result : result,
+                            error : null,
+                            msg:"Details de la vidéo envoyés"
+                        };
+                    } else{
+                        reponse = {
+                            succes: false,
+                            error : err,
+                            msg: "erreur lors du find"
+
+                        };
+                    }
+                    callback(reponse);
+                    db.close();
+                });
+        } else {
+            var reponse = reponse = {
+                succes: false,
+                error : err,
+                msg: "erreur de connexion à la base"
+            };
+            callback(reponse);
+        }
+    });
+};
+
+exports.updateVideo = function(body, callback) {
+    MongoClient.connect(url, function(err, db) {
+        if(!err) {
+            var myquery = { "_id": ObjectId(body.id)};
+            var newvalues = {
+                description : body.description,
+                titre : body.titre,
+                urlimg : body.urlimg,
+                url : body.url,
+                vote:body.vote
+            };
+            db.collection("videos")
+                .updateOne(myquery, newvalues, function(err, result) {
+                    if(!err){
+                        reponse = {
+                            succes : true,
+                            result: result,
+                            msg: "Modification réussie " + result
+                        };
+                    } else {
+                        reponse = {
+                            succes : false,
+                            error : err,
+                            msg: "Problème à la modification"
+                        };
+                    }
+                    callback(reponse);
+                });
+        } else{
+            var reponse = reponse = {
+                succes: false,
+                error : err,
+                msg:"Problème lors de la modification, erreur de connexion."
+            };
+            callback(reponse);
+        }
+    });
+};
+
+exports.findVideos = function(page, pagesize, callback) {
+    MongoClient.connect(url, function(err, db) {
+        console.log("pagesize = " + pagesize);
+        console.log("page = " + page);
+        var selection ={};
+        if(!err){
+            db.collection('videos')
+                .find(selection)
+                .skip(page*pagesize)
+                .limit(pagesize)
+                .toArray()
+                .then(arr => callback(arr));
+        }
+        else{
+            callback(-1);
+        }
+    });
+};
+exports.createVideo = function(formData, callback) {
+	MongoClient.connect(url, function(err, db) {
+	    if(!err) {
+	 
+			let toInsert = {
+				url : formData.url, 
+                description : formData.description,
+                titre:formData.titre,
+                urlimg:formData.urlimg,
+                vote:formData.vote
+			};
+			console.dir(JSON.stringify(toInsert));
+		    db.collection("videos")
+		    .insertOne(toInsert, function(err, result) {
+		    	let reponse;
+
+		        if(!err){
+		            reponse = {
+		                succes : true,
+		                result: result,
+		                error : null,
+		                msg: "Ajout réussi " + result
+		            };
+		        } else {
+		            reponse = {
+		                succes : false,
+		                error : err,
+		                msg: "Problème à l'insertion"
+		            };
+		        }
+		        callback(reponse);
+		    });
+		} else{
+			let reponse = reponse = {
+                    	succes: false,
+                        error : err,
+                        msg:"Problème lors de l'insertion, erreur de connexion."
+                    };
+            callback(reponse);
+		}
+	});
+};
+
+exports.findVideo = function(params, callback) {
+    MongoClient.connect(url, function(err, db) {
+        var selection ={_id: ObjectId(params.id)};
+        if(!err){
+            db.collection('videos')
+                .find(selection)
+                .toArray()
+                .then(arr => callback(arr[0]));
+        }
+        else{
+            callback(-1);
+        }
+    });
+};
+
+exports.deleteVideo = function(params, callback) {
+    MongoClient.connect(url, function(err, db) {
+        if(!err) {
+            let myquery = { "_id": ObjectId(params.id)};
+            let reponse;
+            db.collection("videos")
+                .deleteOne(myquery, function(err, result) {
+                    if(!err){
+                        reponse = {
+                            succes : true,
+                            result: result,
+                            error : null,
+                            msg: "Suppression réussie " + result
+                        };
+                    } else {
+                        console.log("supression resussi");
+                        reponse = {
+                            succes : false,
+                            error : err,
+                            msg: "Problème à  la suppression"
+                        };
+                    }
+                    callback(reponse);
+                });
+        } else{
+            let reponse = reponse = {
+                succes: false,
+                error : err,
+                msg:"Problème lors de la suppression, erreur de connexion."
+            };
+            callback(reponse);
+        }
+    });
+};
---
+++ APINODEJS/server.js
@@ -0,0 +1,132 @@
+const express  = require('express');
+
+var path = require('path');
+const app      = express();
+const port     = process.env.PORT || 3000;
+const server   = require('http').Server(app);
+const mongoDBModule = require('./app_modules/crud-mongo');
+// Pour les formulaires standards
+const bodyParser = require('body-parser');
+// pour les formulaires multiparts
+var multer = require('multer');
+var multerData = multer();
+
+// Cette ligne indique le répertoire qui contient
+// les fichiers statiques: html, css, js, images etc.
+app.use(express.static(path.join(__dirname, '../', 'angular_client/client-video/dist/')));
+// Paramètres standards du module bodyParser
+// qui sert à  récupérer des paramètres reçus
+// par ex, par l'envoi d'un formulaire "standard"
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Methods","POST,DELETE,PUT,GET")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+ });
+
+//------------------
+// ROUTES
+//------------------
+
+// Ici des routes en :
+// http GET (pour récupérer des données)
+// http POST : pour insérer des données
+// http PUT pour modifier des donnÃ©es
+// http DELETE pour supprimer des données
+
+//----------------------------------------------
+// Ces routes forment l'API de l'application !!
+//----------------------------------------------
+
+// Test de la connexion à la base
+app.get('/api/connection', function(req, res) {
+    mongoDBModule.connexionMongo(function(err, db) {
+        var reponse;
+        if(err) {
+            console.log("erreur connexion");
+            reponse = {
+                msg: "erreur de connexion err=" + err
+            }
+        } else {
+            reponse = {
+                msg: "connexion établie"
+            }
+        }
+        res.send(JSON.stringify(reponse));
+    });
+});
+
+// Rend la liste des vidéos
+app.get('/api/videos', function(req, res) {
+    // Si prÃ©sent on prend la valeur du param, sinon 1
+    var page = parseInt(req.query.page || 0);
+    // idem si present on prend la valeur, sinon 10
+    var pagesize = parseInt(req.query.pagesize || 10);
+     mongoDBModule.findVideos(page, pagesize,  function(data) {
+         //console.log(data.JSON);
+        var objdData = {
+            msg:"la liste des videos avec succes",
+            data: data
+        }
+        res.send(JSON.stringify(objdData));
+    });
+});
+
+// Met à jour la vidéo
+// rend un message de réussite si la mise à jour à réussie
+// un message d'erreur sinon
+app.put('/api/video', function (req, res) {
+    mongoDBModule.updateVideo(req.body, function (response) {
+        res.send(JSON.stringify(response));
+    });
+});
+
+// Check si l'url est déjà utilisé
+// rend true si l'url est pas déjà stoké
+app.get('/api/checkurl', function (req, res) {
+    mongoDBModule.checkURL(req.query, function (response) {
+        res.send(JSON.stringify(response));
+    });
+});
+
+// ajoute une nouvelle vidéo en base de données
+/*app.post('/api/videos', function(req, res) {
+    mongoDBModule.createVideo(req.body, function(data) {
+        res.send(JSON.stringify(data));
+    });
+});*/app.post('/api/videos', multerData.fields([]), function(req, res) {
+    console.log("tedi");
+ 	mongoDBModule.createVideo(req.body, function(data) {
+        
+        res.send(JSON.stringify(data)); 
+    });
+});
+
+//Retourne la video dont l'id en param
+app.get('/api/:id/video',function(req, res) {
+    mongoDBModule.findVideo(req.params, function(data) {
+        res.send(JSON.stringify(data));
+    });
+});
+
+//Retourne la video dont l'id en param
+app.delete('/api/video/:id',function(req, res) {
+    mongoDBModule.deleteVideo(req.params, function(data) {
+        res.send(JSON.stringify(data));
+    });
+});
+
+// Retourne l'index de l'application cliente
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../', 'angular_client/client-video/dist/', 'index.html'));
+});
+// ajoute vidéo en base de données
+
+
+// Lance le serveur avec express
+server.listen(port);
+
+console.log("Serveur lancé sur le port : " + port);
