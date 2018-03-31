b372e6c92e38a6d73448d74855da349db1dafe27
boucara <traorebelco@gmail.com>
2018-03-31 18:59:17 +0200

    écran reglage

--- NFCApplicationSmartphone/app/src/main/java/com/mbds/nfcapplicationsmartphone/ConnectionActivity.java
+++ NFCApplicationSmartphone/app/src/main/java/com/mbds/nfcapplicationsmartphone/ConnectionActivity.java
@@ -7,18 +7,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class ConnectionActivity extends AppCompatActivity {
-Button btnInscription;
+Button btnInscription , btnLog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
         btnInscription= (Button) findViewById(R.id.btnIncription);
+        btnLog= (Button) findViewById(R.id.boutonLog);
 
         btnInscription.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ConnectionActivity.this, MainActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+        btnLog.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConnectionActivity.this, ReglageActivity.class);
                 startActivityForResult(intent, 1);
             }
         });
